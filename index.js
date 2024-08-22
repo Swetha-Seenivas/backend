3e6f59a837c357ffaa7f97b759dcc78f44c49e5f
Swetha-Seenivas <swetha.seenivas@anywhere.co>
2024-08-22 22:45:16 +0530

    products list

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+const productsMockData = [
+	{
+		photo:
+			"https://europaeye.com/storage/63055/conversions/SH-742_c01-thumb.jpg",
+		brand: "Scott Harris",
+		modelId: "SH-101",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/83857/conversions/MR-434_c01-thumb.jpg",
+		brand: "Michael Ryen",
+		modelId: "MR-101",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/78601/conversions/SH-846_c02-thumb.jpg",
+		brand: "Scott Harris",
+		modelId: "SH-105",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"	https://europaeye.com/storage/83016/conversions/CIN-5171_c01-thumb.jpg",
+		brand: "Cinzia",
+		modelId: "CIN-102",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/83056/conversions/AT-638_c01-thumb.jpg",
+		brand: "Adin Thomas",
+		modelId: "AT-101",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+
+	{
+		photo:
+			"https://europaeye.com/storage/63055/conversions/SH-742_c01-thumb.jpg",
+		brand: "Scott Harris",
+		modelId: "SH-104",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/81145/conversions/AT-602_c03-thumb.jpg",
+		brand: "Adin Thomas",
+		modelId: "AT-103",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/79926/conversions/CIN-5158_c02-thumb.jpg",
+		brand: "Cinzia",
+		modelId: "CIN-103",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/81397/conversions/AT-614_c02-thumb.jpg",
+		brand: "Adin Thomas",
+		modelId: "AT-104",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/65637/conversions/SH-792_c01-thumb.jpg",
+		brand: "Scott Harris",
+		modelId: "SH-102",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/80142/conversions/MR-414_c02-thumb.jpg",
+		brand: "Michael Ryen",
+		modelId: "MR-102",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/81130/conversions/AT-600_c01-thumb.jpg",
+		brand: "Adin Thomas",
+		modelId: "AT-102",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/80112/conversions/MR-404_c01-thumb.jpg",
+		brand: "Michael Ryen",
+		modelId: "MR-103",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+	{
+		photo:
+			"https://europaeye.com/storage/83004/conversions/CIN-5170_c01-thumb.jpg",
+		brand: "Cinzia",
+		modelId: "CIN-101",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+
+	{
+		photo:
+			"https://europaeye.com/storage/78390/conversions/SH-844_c02-thumb.jpg",
+		brand: "Scott Harris",
+		modelId: "SH-103",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+
+	{
+		photo:
+			"https://europaeye.com/storage/81166/conversions/CIN-5164_c01-thumb.jpg",
+		brand: "Cinzia",
+		modelId: "CIN-104",
+		description: "Stylish mutlicolored full frame spectacles",
+	},
+];
+
+app.get("/", async (req, res) => {
+	res.send({ data: productsMockData });
+});
+
+module.exports = app;
